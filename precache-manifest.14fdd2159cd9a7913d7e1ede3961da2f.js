e5f8fea62004183b772cc2fbd1e20cee2fb0b43d
Maria <murikmarusia@gmail.com>
2020-02-23 20:31:52 +0200

    Updates

---
+++ precache-manifest.14fdd2159cd9a7913d7e1ede3961da2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25ab813ba24825206634dfd4bb5afc76",
+    "url": "/MariaChernienko/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "66372221d7b6a07bfff9",
+    "url": "/MariaChernienko/react_static-list-of-todos/static/css/main.ec9505b9.chunk.css"
+  },
+  {
+    "revision": "425852771245fd8d3008",
+    "url": "/MariaChernienko/react_static-list-of-todos/static/js/2.482519f5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/MariaChernienko/react_static-list-of-todos/static/js/2.482519f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66372221d7b6a07bfff9",
+    "url": "/MariaChernienko/react_static-list-of-todos/static/js/main.15347022.chunk.js"
+  },
+  {
+    "revision": "6f696167451605ec34ff",
+    "url": "/MariaChernienko/react_static-list-of-todos/static/js/runtime-main.ceb19e1e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.56475be9cba24000b69e470ba1f5d46c.js"
+  "/MariaChernienko/react_static-list-of-todos/precache-manifest.14fdd2159cd9a7913d7e1ede3961da2f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MariaChernienko/react_static-list-of-todos/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
